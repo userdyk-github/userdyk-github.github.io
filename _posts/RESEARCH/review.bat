4091c8bc38437c8f62df9f342100b785d8b686d0
userdyk-github <userdyk@gmail.com>
2020-05-05 22:25:11 +0900

    review

---
+++ _posts/RESEARCH/review.bat
@@ -0,0 +1,37 @@
+@echo off
+
+for /l %%i in (1,1,9) do (
+   IF not exist 2019-08-13-PAPER-REVIEW-000%%i.md (
+         copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-000%%i.md
+         echo File 2019-08-13-PAPER-REVIEW-000%%i.md is created!
+         goto :eof
+   )
+)
+
+for /l %%j in (10,1,99) do (
+   IF not exist 2019-08-13-PAPER-REVIEW-00%%j.md (
+         copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-00%%j.md
+         echo File 2019-08-13-PAPER-REVIEW-00%%j.md is created!
+         goto :eof
+   )
+)
+
+
+for /l %%k in (100,1,999) do (
+   IF not exist 2019-08-13-PAPER-REVIEW-0%%k.md (
+         copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-0%%k.md
+         echo File 2019-08-13-PAPER-REVIEW-0%%k.md is created!
+         goto :eof
+   )
+)
+
+for /l %%l in (1000,1,9999) do (
+   IF not exist 2019-08-13-PAPER-REVIEW-%%l.md (
+         copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-%%l.md
+         echo File 2019-08-13-PAPER-REVIEW-%%l.md is created!
+         goto :eof
+   )
+)
+
+@echo success
+  
