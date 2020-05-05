2719e9c1eb84e702bbe06da3d0cbba39ccaa4cad
userdyk-github <userdyk@gmail.com>
2020-05-05 23:35:23 +0900

    create file

--- _posts/RESEARCH/create_reveiw.bat
+++ _posts/RESEARCH/create_reveiw.bat
@@ -9,7 +9,8 @@ for /l %%i in (1,1,9) do (
 	git commit -m "create file"
 	git push
 	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW-000%%i.md
-      	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW.md 
+      	start firefox https://userdyk-github.github.io/research/PAPER-REVIEW-000%%i.html	
+	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW.md 
 	goto :eof
    )
 )
@@ -23,6 +24,7 @@ for /l %%j in (10,1,99) do (
 	git commit -m "create file"
 	git push
 	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW-000%%i.md
+      	start firefox https://userdyk-github.github.io/research/PAPER-REVIEW-00%%j.html	
       	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW.md 
          goto :eof
    )
@@ -38,6 +40,7 @@ for /l %%k in (100,1,999) do (
 	git commit -m "create file"
 	git push
 	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW-000%%i.md
+      	start firefox https://userdyk-github.github.io/research/PAPER-REVIEW-0%%k.html	
       	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW.md 
          goto :eof
    )
@@ -52,6 +55,7 @@ for /l %%l in (1000,1,9999) do (
 	git commit -m "create file"
 	git push
 	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW-000%%i.md
+      	start firefox https://userdyk-github.github.io/research/PAPER-REVIEW-%%l.html	
       	start firefox https://github.com/userdyk-github/userdyk-github.github.io/edit/master/_posts/RESEARCH/2019-08-13-PAPER-REVIEW.md 
          goto :eof
    )
