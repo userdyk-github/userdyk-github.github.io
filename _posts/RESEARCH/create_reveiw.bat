a5b891155b459e6687e9732429561ce4a04147d3
userdyk-github <userdyk@gmail.com>
2020-05-05 23:16:48 +0900

    correct create_review file

--- _posts/RESEARCH/create_reveiw.bat
+++ _posts/RESEARCH/create_reveiw.bat
@@ -4,6 +4,10 @@ for /l %%i in (1,1,9) do (
    IF not exist 2019-08-13-PAPER-REVIEW-000%%i.md (
          copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-000%%i.md
          echo File 2019-08-13-PAPER-REVIEW-000%%i.md is created!
+	git pull
+	git add .
+	git commit -m "create file"
+	git push
          goto :eof
    )
 )
@@ -12,6 +16,10 @@ for /l %%j in (10,1,99) do (
    IF not exist 2019-08-13-PAPER-REVIEW-00%%j.md (
          copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-00%%j.md
          echo File 2019-08-13-PAPER-REVIEW-00%%j.md is created!
+	git pull
+	git add .
+	git commit -m "create file"
+	git push
          goto :eof
    )
 )
@@ -21,6 +29,10 @@ for /l %%k in (100,1,999) do (
    IF not exist 2019-08-13-PAPER-REVIEW-0%%k.md (
          copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-0%%k.md
          echo File 2019-08-13-PAPER-REVIEW-0%%k.md is created!
+	git pull
+	git add .
+	git commit -m "create file"
+	git push
          goto :eof
    )
 )
@@ -29,6 +41,10 @@ for /l %%l in (1000,1,9999) do (
    IF not exist 2019-08-13-PAPER-REVIEW-%%l.md (
          copy 2019-08-13-PAPER-REVIEW-0000.md 2019-08-13-PAPER-REVIEW-%%l.md
          echo File 2019-08-13-PAPER-REVIEW-%%l.md is created!
+	git pull
+	git add .
+	git commit -m "create file"
+	git push
          goto :eof
    )
 )
